43afc71e712385bdd6adb385637b53bd6c499abc
Dejan Jangelovski <76066789+Dejan21@users.noreply.github.com>
2021-08-31 16:57:53 +0200

    Add files via upload

---
+++ components/FAQ.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FAQ = (Question, Answer) => {
+    return (
+        <div className="FAQ">
+            <label htmlFor="Question">Question</label>
+            <input type="text"  />
+            
+           
+        </div>
+    );
+};
+
+export default FAQ;
