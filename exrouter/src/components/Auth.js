60a6922dbcfaedfcc70dea0709fa37d18578c605
Dejan Jangelovski <76066789+Dejan21@users.noreply.github.com>
2021-08-31 17:00:25 +0200

    Add files via upload

---
+++ exrouter/src/components/Auth.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Auth = () => {
+    return (
+        <div className="Auth">
+            <h2>Authentic page</h2>
+        </div>
+    );
+};
+
+export default Auth; 
